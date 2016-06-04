d5862b76bb169d7156d850a443d2b177239cc062
ray <raymundo_rabago@outlook.com>
2016-06-04 12:47:24 -0600

    add structural files

--- gulpfile.js
+++ gulpfile.js
@@ -1,24 +1,46 @@
 var gulp                = require('gulp'),
+    nib                 = require('nib'),
     browserSync         = require('browser-sync'),
     plumber             = require('gulp-plumber'),
     postcss             = require('gulp-postcss'),
-    mqpacker            = require("css-mqpacker"),
+    mqpacker            = require('css-mqpacker'),
     autoprefixer        = require('autoprefixer'),
-    stylus              = require('gulp-stylus');
+    stylus              = require('gulp-stylus'),
+    jade                = require('gulp-jade'),
+    uglify              = require('gulp-uglify'),
+    concat              = require('gulp-concat');
 
 
+// STYLES
 gulp.task('styles', function () {
   var processors = [
     autoprefixer({browsers: ['last 4 version']}),
-    mqpacker
+    mqpacker()
   ];
   gulp.src('./src/css/app.styl')
-    .pipe(stylus())
+    .pipe(stylus({
+        compress: true,
+        use: nib()
+    }))
     .pipe(postcss(processors))
     .pipe(plumber())
     .pipe(gulp.dest('./build/assets/css'));
 });
 
+// SCRIPTS
+gulp.task('js', function() {
+  return gulp.src('./src/js/*.js')
+    .pipe( uglify() )
+    .pipe( concat('scripts.min.js'))
+    .pipe( gulp.dest('./build/assets/js/'));
+});
+
+// TEMPLATES
+gulp.task('views', function() {
+  return gulp.src('src/*.jade')
+    .pipe( jade({ pretty: true }))
+    .pipe( gulp.dest('build/'));
+});
 
 // SERVER
 gulp.task('server', function() {
@@ -30,13 +52,12 @@ gulp.task('server', function() {
     });
 });
 
-
 // WATCH
 gulp.task('watch', function () {
     gulp.watch(['./src/**/**/*.styl'], ['styles']);
-    gulp.watch("./*.html").on('change', browserSync.reload);
+    gulp.watch('./src/**/*.jade',['views']);
+    gulp.watch("./build/*.html").on('change', browserSync.reload);
 });
-
 
 // DEFAULT
 gulp.task('default', ['watch', 'server']);
